675287016f98a4b0059f2c34bd52e517bec700be
Pexel14 <159523974+Pexel14@users.noreply.github.com>
2024-02-12 10:49:01 +0100

    Update PruebasEGit.java Remoto

--- PruebasEGit/src/PruebasEGit/PruebasEGit.java
+++ PruebasEGit/src/PruebasEGit/PruebasEGit.java
@@ -5,11 +5,8 @@ public class PruebasEGit {
 	public static void main(String[] args) {
 		System.out.println("PruebasEGit");
 		System.out.println("Segundo commit master");
-		
 		System.out.println("Primer commit rama1");
-
 		System.out.println("Tercer commit master");
-		
 		System.out.println("Segundo commit rama1");
 	}
 
